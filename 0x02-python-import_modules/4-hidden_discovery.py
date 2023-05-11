6db00a791e9f57b5fdac399ea247e83e194426f8
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-11 13:39:05 -0700

    Adding 4-hidden_discovery.py task solution

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    for i in dir(hidden_4):
+    n = dir(hidden_4)
+    for i in n:
         if i[:2] != "__":
-            print("{:s}".format(i))
+            print(i)
