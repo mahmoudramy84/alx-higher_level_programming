611edb2b6556f3593507f1c16d6d82a287727c37
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-14 05:17:51 -0700

    Updating 6-print_matrix_integer.py task solution

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for column in row:
-            if column == row[-1]:
-                print("{:d}".format(column), end="")
+        for item in row:
+            if item == row[-1]:
+                print("{:d}".format(item), end="")
             else:
-                print("{:d}".format(column), end" ")
+                print("{:d}".format(item), end=" ")
         print('')
