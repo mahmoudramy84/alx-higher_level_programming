c77066870ccdd01c0a1d2ab6e940eddd5fcee30f
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-14 05:06:42 -0700

    Adding 6-print_matrix_integer.py task solution

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in matrix i:
+        for j in i:
             if j == i[-1]:
                 print("{:d}".format(j), end="")
             else:
