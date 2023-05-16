f60aef5c966fd8b2d5b2ef08243ce426a10a213e
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-16 01:46:05 -0700

    Updating 100-my_calculator.py task soluion

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,12 +4,11 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == "__main__":
-    n = len(argv)
-
-    if n != 4:
+    length = len(argv)
+    if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if n == 4:
+    if length == 4:
         operator = argv[2]
         a = int(argv[1])
         b = int(argv[3])
@@ -21,6 +20,6 @@ if __name__ == "__main__":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif operator == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
