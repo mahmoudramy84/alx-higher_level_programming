d0b863fe038dae84eded3133af854f6b4a02f795
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-16 00:59:41 -0700

    Updating 2-args.py task solution

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print("{:d} arguments: ".format(count))
-    for i in range(count - 1):
-        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments: ".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
