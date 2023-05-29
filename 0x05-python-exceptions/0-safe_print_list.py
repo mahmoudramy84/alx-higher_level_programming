681a43bd1edc538e8bc7680550597f8c2da85abb
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-29 04:11:37 -0700

    adding 0-safe_print_list.py task solution

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
             print(f"{mylist[i]}", end="")
             count += 1
