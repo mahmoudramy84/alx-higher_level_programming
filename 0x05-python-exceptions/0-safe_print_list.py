c768c232597976766828b5081b715e158d6a9e38
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-29 04:12:59 -0700

    Updating 0-safe_print_list.py task solution

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{mylist[i]}", end="")
             count += 1
-        except Exception:
+        except IndexError:
             braek
     print()
     return(count)
