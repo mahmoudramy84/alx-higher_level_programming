b85911f2f65847ba75031575e0031b1a693d1cb6
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-30 03:37:04 -0700

    adding 2-square.py task solution

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a square with Private instance attribute: size"""
+
+
+class Square:
+    """Private instance attribute: size"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError ("size must be >= 0")
+
