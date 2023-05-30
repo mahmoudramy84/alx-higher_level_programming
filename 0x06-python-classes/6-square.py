a583d8fa6ca28086181f71e24b8fc6432664a155
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-30 06:02:10 -0700

    adding 6-square.py task solution

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
+# 6-square.py
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
             size (int): The size of the new square.
-            position (tuple): The position of the new square
+            position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get the current size of the square."""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Set the current size of the square."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-     @property
+    @property
     def position(self):
-        """Get the current position of the square."""
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
     def position(self, value):
-        """Set the current position of the square."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -44,11 +44,11 @@ class Square:
 
     def area(self):
         """Return the current area of the square."""
-        return self.__size ** 2
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """prints in stdout the square with character #"""
-        if self.size == 0:
+        """Print the square with the # character."""
+        if self.__size == 0:
             print("")
             return
 
