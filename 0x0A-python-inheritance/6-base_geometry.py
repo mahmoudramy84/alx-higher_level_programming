43ec18c3836a22d2ef381262ceb306031b713a99
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-06-12 15:14:58 +0300

    adding 6-base_geometry.py task solution

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """class represents a base geometry"""
+
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
