60b115754f1d597bc8e65662a5772b4391c799b7
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-06-12 19:49:35 +0300

    adding 8-rectangle.py task solution

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,9 +5,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """a class to define rectangle using BaseGeometry"""
-  
+
     def __init__(self, width, height):
-        """Intialize anew rectangle"""
+        """Intialize anew rectangle
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
