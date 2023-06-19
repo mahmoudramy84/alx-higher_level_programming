51b380bc37f5d5c37428c83d33966ba7d13a546b
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-06-19 20:23:45 +0300

    Adding models/square.py task file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = None
+        super().__init__(size, size, x, y, id)
