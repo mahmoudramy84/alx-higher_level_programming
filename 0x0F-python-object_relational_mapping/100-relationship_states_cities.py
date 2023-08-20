cc85b6d2da8a7add8921deb5ce043cf529f24478
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-08-20 16:08:06 +0300

    added relationship_city.py relationship_state.py 100-relationship_states_cities.py task files solutions

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
+"""
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    
+    Base.metadata.create_all(engine)
+    session_maker = sessionmaker(bind=engine)
+
+    session = session_maker()
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
+
+    session.add(cal_state)
+    session.commit()
+    session.close()
