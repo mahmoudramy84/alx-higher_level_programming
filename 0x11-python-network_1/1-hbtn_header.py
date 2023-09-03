d963ed410a956c886e3c5c4e38b4697a27a71350
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-09-03 12:12:51 +0300

    Updated 1-hbtn_header.py task solution

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays 
+takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response.
 """
 
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     request = Request(url)
     with urlopen(request) as responce:
         head = (dict(responce.headers).get("X-Request-Id"))
-        print (head)
+        print(head)
