57571efd35bbd038607d6e82b1890ebd618c12fc
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-09-03 13:23:08 +0300

    added 100-github_commits.py task solytion

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,10 @@ from requests import get
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+        argv[2], argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
+    res = get(url)
+    commits = res.json()
     try:
         for i in range(10):
             print("{}: {}".format(
