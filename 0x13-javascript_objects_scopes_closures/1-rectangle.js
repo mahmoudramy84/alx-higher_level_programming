3984ad1c2cb26ba5fa163966293e9bf8e033b3f2
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-01-09 16:07:39 +0200

    adding 1-rectangle.js task solution

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-/*Write a class Rectangle that defines a rectangle:
-
+/* Write a class Rectangle that defines a rectangle:
 You must use the class notation for defining your class
 The constructor must take 2 arguments w and h
 Initialize the instance attribute width with the value of w
