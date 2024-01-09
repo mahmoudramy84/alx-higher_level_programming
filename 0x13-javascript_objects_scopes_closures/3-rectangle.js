fa0de311a640663fa17b81edffa94d6c5307a259
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-01-09 21:54:33 +0200

    adding 3-rectangle.js task solution

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -14,5 +14,13 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let j = 0; j < this.width; j++) rect += 'X';
+      console.log(rect);
+    }
+  }
 }
 module.exports = Rectangle;
