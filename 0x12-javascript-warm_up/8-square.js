24edb3d97f21d635be0317a6f5696c6349ef0725
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-01-10 11:05:20 +0200

    Updating 8-square.js task solution

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const x = process.argv[2];
-for (let row = 0; row < x; row++) {
-  let square = '';
-  for (let colm = 0; colm < x; colm++) {
-    square += 'X';
+if (parseInt(x)) {
+  for (let row = 0; row < x; row++) {
+    let square = '';
+    for (let colm = 0; colm < x; colm++) {
+      square += 'X';
+   }
+    console.log(square);
   }
-  console.log(square);
+} else {
+    console.log('Missing size');
 }
