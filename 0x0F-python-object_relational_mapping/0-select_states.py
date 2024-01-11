a0938c32676d082baafc1d542ca9863594fa8551
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-01-11 09:24:09 +0200

    adding 0-select_states.py task solution

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa:
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """ Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
